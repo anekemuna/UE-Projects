ded7620181e0d2fd001a47ffd0eaf03be03b052f
Munachimso Aneke <52995219+anekemuna@users.noreply.github.com>
2021-12-18 15:52:39 -0600

    Buffer Definitions

---
+++ CS470/P2_Thread_Sync/buffer.h
@@ -0,0 +1,12 @@
+// Munachimso Aneke
+// Project 2 - thread Synchronization
+// buffer.h file
+
+#ifndef BUFFER_H_INCLUDED
+#define BUFFER_H_INCLUDED
+
+
+typedef int buffer_item;
+#define BUFFER_SIZE 5
+
+#endif //BUFFER_H_INCLUDED
